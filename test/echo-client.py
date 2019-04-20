b4b910fd55e6198b1db7d91e71b392e3893f5165
Kai Zhang <kai.zhang01@@duke.edu>
2019-04-20 17:16:30 -0400

    add .proto file

---
+++ test/echo-client.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+
+import socket
+
+HOST = socket.gethostname()
+PORT = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Hello, world')
+    data = s.recv(1024)
+
+print('Received', repr(data))
