bfd32fd61a755ee2d603aa3110f2367eb4fe1736
Kai Zhang <kai.zhang01@@duke.edu>
2019-04-22 16:38:23 -0400

    regular backup

--- ups.py
+++ ups.py
@@ -1,4 +1,5 @@
 import socket
+import select
 import sys
 import time
 import psycopg2
@@ -7,17 +8,27 @@ from google.protobuf.internal.encoder import _EncodeVarint
 from proto import world_ups_pb2
 from proto import ups_amazon_pb2
 import build_commands
-import ups_amazon_commands
+import build_ups_amazon_commands
 import threading
+from handle_request import *
 
 # UPS_HOST = socket.gethostname()
 # UPS_PORT = 54321
-WORLD_HOST = 'vcm-9448.vm.duke.edu'
+WORLD_HOST = 'vcm-9229.vm.duke.edu'
 WORLD_PORT = 12345
-AMAZON_HOST = ''
-AMAZON_POST = ''
+AMAZON_HOST = 'vcm-9448.vm.duke.edu'
+AMAZON_PORT = 12345
 
-SEQNUM = 0;
+ack_set = set()
+# ack_set.add(x)
+# ack_set.remove(x)
+
+sleep_time = 5
+
+dbcursor = 0
+
+world_seqnum = 0
+amazon_seqnum = 0
 NUM_TRUCK_INIT = 100
 
 # To do list:
@@ -32,15 +43,14 @@ NUM_TRUCK_INIT = 100
 # 8. Generate Package ID
 # 9. Send go Deliver to trucks.
 
-
 # Connect to Database.
 def connect_db():
     while True:
         try:
             dbconn = psycopg2.connect(
-                database = 'wgzyjrdk',
-                user = 'wgzyjrdk',
-                password = 'NdCR_HW8uL-E-id_IUpNrn_cLNqEt593',
+                database = 'xwqawgaa',
+                user = 'xwqawgaa',
+                password = 'nmErOf1YehpHpldPYghZpaNTdu_RIxUJ',
                 host = 'isilo.db.elephantsql.com',
                 port = '5432')
             # cur = dbconn.cursor()
@@ -67,7 +77,7 @@ def connect_world_server():
 def connect_world(world_socket, worldid, num_truck_init):
     u_connect = world_ups_pb2.UConnect()
     u_connect.isAmazon = False
-    if world_id:
+    if worldid:
         u_connect.worldid = int(worldid)
         send_msg(world_socket, u_connect)
     else:
@@ -92,8 +102,8 @@ def recv_world(world_socket):
     return u_responses
 
 # Receive AUCommands from Amazon
-def recv_amazon(world_socket):
-    response = recv_msg(world_socket)
+def recv_amazon(amazon_socket):
+    response = recv_msg(amazon_socket)
     au_commands = ups_amazon_pb2.AUCommands()
     au_commands.ParseFromString(response)
     return au_commands
@@ -134,22 +144,42 @@ def recv_msg(s):
     return whole_message
 
 # Handle messages from world
-def handle_world(world_socket):
+def handle_world(amazon_socket, world_socket):
     # Read from world_socket
     u_response = recv_world(world_socket)
     for completion in u_responses.completions:
-        handle_completion(completion)
-        print('Handle completions')
-        t = threading.Thread(target=f())
-        t.start
+        t = threading.Thread(target = handle_completion(amazon_socket, world_socket, completion))
+        t.start()
     for delivered in u_responses.delivered:
-        print('Handle delivered')
+        t = threading.Thread(target = handle_delivered(amazon_socket, world_socket, delivered))
+        t.start()
     for ack in u_responses.acks:
-        print('Handle acks')
+        ack_set.add(ack)
     for truckstatus in u_responses.truckstatus:
-        print('Handle truckstatus')
+        t = threading.Thread(target = handle_truckstatus(amazon_socket, world_socket, truckstatus))
+        t.start()
     for error in u_responses.error:
-        print('Handle error')
+        t = threading.Thread(target = handle_error(amazon_socket, world_socket, error))
+        t.start()
+
+# Handle messages from amazon
+def handle_amazon(amazon_socket, world_socket):
+    # Read from amazon_socket
+    au_commands = recv_amazon(amazon_socket)
+    print(au_commands)
+    for warehouse in au_commands.warehouses:
+        print('Handle warehouse')
+        global amazon_seqnum
+        global world_seqnum
+        t = threading.Thread(target = execute_gopickups(amazon_socket, world_socket, warehouse, amazon_seqnum, world_seqnum))
+        amazon_seqnum += 1
+        world_seqnum += 1
+        t.start()
+    for dest in au_commands.dests:
+        t = threading.Thread(execute_godelivery(amazon_socket, world_socket, dest))
+        t.start()
+    for ack in au_commands.acks:
+        print('Handle acks')
     # Do something...
 
 # while True:
@@ -182,17 +212,20 @@ def main():
     amazon_socket = connect_amazon()
     init_world = ups_amazon_pb2.InitWorld()
     init_world.worldid = worldid
-    init_world.seqnum = SEQNUM
-    SEQNUM += 1
+    # init_world.seqnum = amazon_seqnum
+    # amazon_seqnum += 1
+    init_world.seqnum = 1
     send_msg(amazon_socket, init_world)
 
+    """
     response = recv_msg(amazon_socket)
     au_commands = ups_amazon_pb2.AUCommands();
     au_commands.ParseFromString(response)
     for ack in au_commands.acks:
         if ack == init_world.seqnum:
             break
-
+    """
+    
     # Repeatedly read from world or amazon
     inputs = [world_socket, amazon_socket]
     while True:
@@ -200,37 +233,14 @@ def main():
         if len(infds) != 0:
             for fds in infds:
                 if fds is world_socket:
-                    # Read from world_socket
-                    u_responses = recv_world(world_socket)
-                    for completion in u_responses.completions:
-                        # handle_completions()
-                        print('Handle completions')
-                        t = threading.Thread(target=f())
-                        t.start
-                    for delivered in u_responses.delivered:
-                        print('Handle delivered')
-                    for ack in u_responses.acks:
-                        print('Handle acks')
-                    for truckstatus in u_responses.truckstatus:
-                        print('Handle truckstatus')
-                    for error in u_responses.error:
-                        print('Handle error')
-                    # Do something...
+                    handle_world(amazon_socket, world_socket)
                 else:
-                    # fds is amazon_socket
-                    # Read from amazon_socket
-                    au_commands = recv_amazon(world_socket)
-                    for warehouse in au_commands.warehouses:
-                        execute_gopickups(amazon_socket, world_socket, warehouse)
-                    for dest in au_commands.dests:
-                        execute_godelivery(amazon_socket, world_socket, dest)
-                    for ack in au_commands.acks:
-                        print('Handle acks')
-                    # Do something...
+                    print('Received message from Amazon')
+                    handle_amazon(amazon_socket, world_socket)
     
     # Connect to database
     dbconn = connect_db()
-    cur = dbconn.cursor()
+    dbcursor = dbconn.cursor()
 
     world_socket.close()
     cur.close()
